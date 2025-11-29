ea50658d2c028de8d41b2aa56f24811b76348623
Bonsen Wakjira <bonsenw@gmail.com>
2025-11-29 19:30:58 +1100

    Renaming movie app

--- proj/movie-search/src/App.jsx
+++ proj/movie-search/src/App.jsx
@@ -3,7 +3,6 @@ import Search from './components/Search'
 import Spinner from './components/Spinner'
 import MovieCard from './components/MovieCard'
 import { useDebounce } from 'react-use'
-import http from 'http'
 
 const API_BASE_URL = "https://api.themoviedb.org/3"
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY
